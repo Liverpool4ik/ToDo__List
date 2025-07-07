1692acc5adafb71796633df250431c39088621d7
Liverpool4ik <liverpool4ik@proton.me>
2025-07-07 16:08:09 +0300

    26. toDo-5. isChecked - to delete

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ export const App = () => {
 
    //setTodo ==> setNewTask
    const [todoList, setNewTask] = useState([
-      { id: 1, name: "Купить шкаф Валерке" },
-      { id: 2, name: "Установить золотые ручки на ящкики (6шт.) - белый шкаф" },
-      { id: 3, name: "Измерить ДВчП в детском ящике" },
-      { id: 4, name: "Заказать ДВП в АБРИСЕ" },
-      { id: 5, name: "Измерить кусочки ДСП в ящиках десткого шкафа (накладки) - 4 шт." },
-      { id: 6, name: "Заказать накладки для ящиков (4шт.) в АБРИСЕ" },])
+      { id: 1, name: "Купить шкаф Валерке", isChecked: false, },
+      { id: 2, name: "Установить золотые ручки на ящкики (6шт.) - белый шкаф", isChecked: false, },
+      { id: 3, name: "Измерить ДВП в детском ящике", isChecked: true, },
+      { id: 4, name: "Заказать ДВП в АБРИСЕ", isChecked: false, },
+      { id: 5, name: "Измерить кусочки ДСП в ящиках десткого шкафа (накладки) - 4 шт.", isChecked: false, },
+      { id: 6, name: "Заказать накладки для ящиков (4шт.) в АБРИСЕ", isChecked: false, },])
 
 
    //setTodo ==> setNewTask
@@ -21,7 +21,7 @@ export const App = () => {
       if (inputValue) {
          // проверка -- if -- сделана для того чтобы добавлять только в том случае, когда в поле инпута хоть что-то введено.... а не пустая строка...null, undefined и не 0 и не false значение. 
 
-         setNewTask([...todoList, { id: Math.random(), name: inputValue }])
+         setNewTask([...todoList, { id: Math.random(), name: inputValue, isChecked: false, }])
          //  ====>>> МОЖНО И НЕ ПИСАТЬ ЭТУ ПЕРЕМННУЮ --- А СРАЗУ ===>>>  ОТПРАВИТЬ НОВЫЙ МАССИВ ЧЕРЕЗ (setNewTask) <<<======
          // setNewTask([...todoList, "сделать"])
 
@@ -38,7 +38,7 @@ export const App = () => {
    const inputHandler = (event) => {
       setInputValue(event.target.value)
    }
-
+   console.log(todoList);
    return (
       <div className="App">
          <div className="topInputAddBlock">
@@ -53,10 +53,13 @@ export const App = () => {
             </button>
          </div>
 
-         {todoList.map((i) => (
+         {todoList.map((item) => (
             <div className="block">
-               <div>{i.name}</div>
-               <div onClick={() => deleteHandler(i.id)}>X</div>
+               <div className="blockName">
+                  <input type="checkbox" checked={item.isChecked} />
+                  <div className="doneLinedItem">{item.name}</div>
+               </div>
+               <div onClick={() => deleteHandler(item.id)}>X</div>
             </div>
          ))}
       </div>
